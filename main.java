3a488c79c35eb24a007c49f59149dfa171935d70
girishc007 <girishchaudhari2003@gmail.com>
2026-07-22 22:17:38 +0530

    Intial Commit

---
+++ main.java
@@ -0,0 +1,20 @@
+import java.util.io;
+import java.util.*;
+
+
+class demo
+{
+    public void read()
+    {
+        System.out.println("Hello this is my simple program");
+    }
+}
+
+class main
+{
+    public static void main(String []args)
+    {
+        demo d1 = new demo()
+        d1.read();
+    }
+}
